a7bf882c07cb60194d17a4d688368e87e48b7f4e
ndlorenc <ndlorenc@icloud.com>
2024-09-10 15:43:10 -0400

    [Team: ndlorenc, jfmcdavi, tndo] Re-ran autopep8 on hw2_debugging.py after fixing code

--- hw2/hw2_debugging.py
+++ hw2/hw2_debugging.py
@@ -7,7 +7,6 @@ the merge sort to print out the sorted array.
 import rand
 
 
-
 def merge_sort(arr):
     """
     This is a recursive function to split the
@@ -28,14 +27,13 @@ def recombine(left_arr, right_arr):
     merge_arr = [None] * (len(left_arr) + len(right_arr))
     while left_index < len(left_arr) and right_index < len(right_arr):
         if left_arr[left_index] < right_arr[right_index]:
-            
+
             merge_arr[left_index + right_index] = left_arr[left_index]
             left_index += 1
         else:
-            
+
             merge_arr[left_index + right_index] = right_arr[right_index]
             right_index += 1
-            
 
     for i in range(right_index, len(right_arr)):
         merge_arr[left_index + right_index] = right_arr[i]
