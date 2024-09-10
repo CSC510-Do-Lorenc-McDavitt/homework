82e601689f1240c7037e4f40afa773c8dfe4628e
ndlorenc <ndlorenc@icloud.com>
2024-09-10 14:50:00 -0400

    [Team: ndlorenc, jfmcdavi, tndo] Ran all 3 tools on rand.py

--- hw2/rand.py
+++ hw2/rand.py
@@ -1,8 +1,15 @@
+"""
+File that includes a function to creates a
+random array using a Unix subprocess function.
+"""
 import subprocess
 
+
 def random_array(arr):
+    "Creates a random array using a Unix subprocess function."
     shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
-        arr[i] = int(shuffled_num.stdout)
+    for index, _value in enumerate(arr):
+        shuffled_num = subprocess.run(
+            ["shuf", "-i1-20", "-n1"], capture_output=True, check=False)
+        arr[index] = int(shuffled_num.stdout)
     return arr
