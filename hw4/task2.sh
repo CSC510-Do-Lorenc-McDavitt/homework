a95f6c512958a0ea31cd08f30db72d19a0ef3a40
tndo <tndo@ncsu.edu>
2024-10-09 17:13:15 -0400

    [tndo][team] task2 script

---
+++ hw4/task2.sh
@@ -0,0 +1,1 @@
+grep -l "sample" -r dataset1/ | uniq | xargs grep "CSC510" | cut -d: -f1 | uniq -c | grep -E "^\s*([3-9]|[0-9]{2,})" | gawk '{print $2}' | xargs ls -l | gawk '{print $5 " " $9 }' | sort -k1 -r -n | cut -d ' ' -f2 | xargs grep "CSC510" | cut -d: -f1 | uniq -c | sort -k1 -r -n | gawk '{print $2}' | sed 's/file_/filtered_/' | cut -d/ -f2
