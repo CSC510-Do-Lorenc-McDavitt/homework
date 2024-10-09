e042c4c948bfaa01465a552850e4a4a3203ffd27
jfmcdavitt <mcdavittjacob@gmail.com>
2024-10-09 18:25:53 -0400

    [jfmcdavi] Continued work on task 3, got the average age

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-grep -E "S$" titanic.csv | grep -E "^[0-9]*,[0-9],2" |  sed 's/,female,/,F,/' | sed 's/,male,/,M,/' | gawk -F, '{ sum += $7 } END { print sum }'
-
+grep -E "S$" titanic.csv | grep -E "^[0-9]*,[0-9],2" |  sed 's/,female,/,F,/' | sed 's/,male,/,M,/' | grep "[MF],[0-9]" | gawk -F, '{ sum += $7 } END {print sum / NR }'
